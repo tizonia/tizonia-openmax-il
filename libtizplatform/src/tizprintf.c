6d2bec3479631415af4c216ba1f4e759219241ff
Juan A. Rubio <jarubio2001@gmail.com>
2014-06-28 01:53:11 +0100

    libtizplatform: added TIZ_PRINTF macros

---
+++ libtizplatform/src/tizprintf.c
@@ -0,0 +1,69 @@
+/**
+ * Copyright (C) 2011-2014 Aratelia Limited - Juan A. Rubio
+ *
+ * This file is part of Tizonia
+ *
+ * Tizonia is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Tizonia is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Tizonia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file   tizlog.h
+ * @author Juan A. Rubio <juan.rubio@aratelia.com>
+ *
+ * @brief  Tizonia Platform - tiz_printf implementation
+ *
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <time.h>
+#include <alloca.h>
+
+#include "tizprintf.h"
+
+void tiz_printf (const char *ap_color, const char *ap_file, int a_line,
+                 const char *ap_func, const char *ap_format, ...)
+{
+  size_t size = 256;
+  char *buffer = alloca (size);
+  va_list va;
+  va_start (va, ap_format);
+  vsnprintf (buffer, size, ap_format, va);
+  va_end (va);
+  if (ap_file && ap_func)
+    {
+      const char *p_env = getenv("TIZONIA_DEBUG_COLOR_TRACE");
+      if (NULL != p_env && strncmp (p_env, "1", 2) == 0)
+        {
+          fprintf (stderr, "%s[%s:%s:%d] --- %s%s\n", ap_color, ap_file, ap_func,
+                   a_line, buffer, KNRM);
+        }
+    }
+  else
+    {
+      fprintf (stderr, "%s%s%s", ap_color, buffer, KNRM);
+    }
+}
---
+++ libtizplatform/src/tizprintf.h
@@ -0,0 +1,109 @@
+/**
+ * Copyright (C) 2011-2014 Aratelia Limited - Juan A. Rubio
+ *
+ * This file is part of Tizonia
+ *
+ * Tizonia is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Tizonia is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Tizonia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file   tizlog.h
+ * @author Juan A. Rubio <juan.rubio@aratelia.com>
+ *
+ * @brief  Tizonia Platform - printf macros
+ *
+ *
+ */
+
+#ifndef TIZPRINTF_H
+#define TIZPRINTF_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+#define TIZ_PRINTF_RED(format, args...)                 \
+  tiz_printf(KRED, NULL, -1, NULL, format, ##args);
+
+#define TIZ_PRINTF_GRN(format, args...)                 \
+  tiz_printf(KGRN, NULL, -1, NULL, format, ##args);
+
+#define TIZ_PRINTF_YEL(format, args...)                 \
+  tiz_printf(KYEL, NULL, -1, NULL, format, ##args);
+
+#define TIZ_PRINTF_BLU(format, args...)                 \
+  tiz_printf(KBLU, NULL, -1, NULL, format, ##args);
+
+#define TIZ_PRINTF_MAG(format, args...)                 \
+  tiz_printf(KMAG, NULL, -1, NULL, format, ##args);
+
+#define TIZ_PRINTF_CYN(format, args...)                 \
+  tiz_printf(KCYN, NULL, -1, NULL, format, ##args);
+
+#define TIZ_PRINTF_WHT(format, args...)                 \
+  tiz_printf(KWHT, NULL, -1, NULL, format, ##args);
+
+
+#ifdef NDEBUG
+
+#define TIZ_PRINTF_DBG_RED(format, args...)
+#define TIZ_PRINTF_DBG_GRN(format, args...)
+#define TIZ_PRINTF_DBG_YEL(format, args...)
+#define TIZ_PRINTF_DBG_BLU(format, args...)
+#define TIZ_PRINTF_DBG_MAG(format, args...)
+#define TIZ_PRINTF_DBG_CYN(format, args...)
+#define TIZ_PRINTF_DBG_WHT(format, args...)
+
+#else
+
+#define TIZ_PRINTF_DBG_RED(format, args...)                             \
+  tiz_printf(KRED, __FILE__, __LINE__, __FUNCTION__, format, ##args);
+
+#define TIZ_PRINTF_DBG_GRN(format, args...)                             \
+  tiz_printf(KGRN, __FILE__, __LINE__, __FUNCTION__, format, ##args);
+
+#define TIZ_PRINTF_DBG_YEL(format, args...)                             \
+  tiz_printf(KYEL, __FILE__, __LINE__, __FUNCTION__, format, ##args);
+
+#define TIZ_PRINTF_DBG_BLU(format, args...)                             \
+  tiz_printf(KBLU, __FILE__, __LINE__, __FUNCTION__, format, ##args);
+
+#define TIZ_PRINTF_DBG_MAG(format, args...)                             \
+  tiz_printf(KMAG, __FILE__, __LINE__, __FUNCTION__, format, ##args);
+
+#define TIZ_PRINTF_DBG_CYN(format, args...)                             \
+  tiz_printf(KCYN, __FILE__, __LINE__, __FUNCTION__, format, ##args);
+
+#define TIZ_PRINTF_DBG_WHT(format, args...)                             \
+  tiz_printf(KWHT, __FILE__, __LINE__, __FUNCTION__, format, ##args);
+
+#endif
+
+void tiz_printf (const char *ap_color, const char *ap_file, int a_line, const char *ap_func,
+                 const char *ap_format, ...);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TIZPRINTF_H */
