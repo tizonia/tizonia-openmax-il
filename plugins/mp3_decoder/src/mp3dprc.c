d16b7c0a8e42e18f8b1234ede348ea039ba37c2e
Juan A. Rubio <jarubio2001@gmail.com>
2015-07-20 00:39:42 +0100

    tizmp3dec: Fixed licensing details (was GPLv3, should be GPLv2)

--- plugins/mp3_decoder/src/mp3dprc.c
+++ plugins/mp3_decoder/src/mp3dprc.c
@@ -3,10 +3,10 @@
  *
  * This file is part of Tizonia
  *
- * Tizonia is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Tizonia is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
  * Tizonia is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
