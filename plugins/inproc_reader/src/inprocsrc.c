008d2a07996b68ba90da0762a755cc2236b5a5bd
Juan A. Rubio <jarubio2001@gmail.com>
2015-10-12 20:00:31 +0100

    tizinprocsrc: initial version

---
+++ plugins/inproc_reader/src/inprocsrc.c
@@ -0,0 +1,203 @@
+/**
+ * Copyright (C) 2011-2015 Aratelia Limited - Juan A. Rubio
+ *
+ * This file is part of Tizonia
+ *
+ * Tizonia is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Tizonia is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Tizonia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file   inprocsrc.c
+ * @author Juan A. Rubio <juan.rubio@aratelia.com>
+ *
+ * @brief  Tizonia OpenMAX IL - ZMQ inproc socket reader
+ *
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <assert.h>
+#include <string.h>
+
+#include <OMX_Core.h>
+#include <OMX_Component.h>
+#include <OMX_Types.h>
+
+#include <tizplatform.h>
+
+#include <tizport.h>
+#include <tizscheduler.h>
+
+#include "inprocsrcprc.h"
+#include "inprocsrc.h"
+
+#ifdef TIZ_LOG_CATEGORY_NAME
+#undef TIZ_LOG_CATEGORY_NAME
+#define TIZ_LOG_CATEGORY_NAME "tiz.inproc_reader"
+#endif
+
+static OMX_VERSIONTYPE inproc_reader_version = { {1, 0, 0, 0} };
+
+static OMX_PTR
+instantiate_audio_port (OMX_HANDLETYPE ap_hdl)
+{
+  tiz_port_options_t port_opts = {
+    OMX_PortDomainAudio,
+    OMX_DirOutput,
+    ARATELIA_INPROC_READER_PORT_MIN_BUF_COUNT,
+    ARATELIA_INPROC_READER_PORT_MIN_BUF_SIZE,
+    ARATELIA_INPROC_READER_PORT_NONCONTIGUOUS,
+    ARATELIA_INPROC_READER_PORT_ALIGNMENT,
+    ARATELIA_INPROC_READER_PORT_SUPPLIERPREF,
+    {ARATELIA_INPROC_READER_PORT_INDEX, NULL, NULL, NULL},
+    -1                          /* use -1 for now */
+  };
+
+  return factory_new (tiz_get_type (ap_hdl, "tizbinaryport"),
+                      &port_opts);
+}
+
+static OMX_PTR
+instantiate_video_port (OMX_HANDLETYPE ap_hdl)
+{
+  tiz_port_options_t port_opts = {
+    OMX_PortDomainVideo,
+    OMX_DirOutput,
+    ARATELIA_INPROC_READER_PORT_MIN_BUF_COUNT,
+    ARATELIA_INPROC_READER_PORT_MIN_BUF_SIZE,
+    ARATELIA_INPROC_READER_PORT_NONCONTIGUOUS,
+    ARATELIA_INPROC_READER_PORT_ALIGNMENT,
+    ARATELIA_INPROC_READER_PORT_SUPPLIERPREF,
+    {ARATELIA_INPROC_READER_PORT_INDEX, NULL, NULL, NULL},
+    -1                          /* use -1 for now */
+  };
+
+  return factory_new (tiz_get_type (ap_hdl, "tizbinaryport"),
+                      &port_opts);
+}
+
+static OMX_PTR
+instantiate_image_port (OMX_HANDLETYPE ap_hdl)
+{
+  tiz_port_options_t port_opts = {
+    OMX_PortDomainImage,
+    OMX_DirOutput,
+    ARATELIA_INPROC_READER_PORT_MIN_BUF_COUNT,
+    ARATELIA_INPROC_READER_PORT_MIN_BUF_SIZE,
+    ARATELIA_INPROC_READER_PORT_NONCONTIGUOUS,
+    ARATELIA_INPROC_READER_PORT_ALIGNMENT,
+    ARATELIA_INPROC_READER_PORT_SUPPLIERPREF,
+    {ARATELIA_INPROC_READER_PORT_INDEX, NULL, NULL, NULL},
+    -1                          /* use -1 for now */
+  };
+
+  return factory_new (tiz_get_type (ap_hdl, "tizbinaryport"),
+                      &port_opts);
+}
+
+static OMX_PTR
+instantiate_other_port (OMX_HANDLETYPE ap_hdl)
+{
+  tiz_port_options_t port_opts = {
+    OMX_PortDomainOther,
+    OMX_DirOutput,
+    ARATELIA_INPROC_READER_PORT_MIN_BUF_COUNT,
+    ARATELIA_INPROC_READER_PORT_MIN_BUF_SIZE,
+    ARATELIA_INPROC_READER_PORT_NONCONTIGUOUS,
+    ARATELIA_INPROC_READER_PORT_ALIGNMENT,
+    ARATELIA_INPROC_READER_PORT_SUPPLIERPREF,
+    {ARATELIA_INPROC_READER_PORT_INDEX, NULL, NULL, NULL},
+    -1                          /* use -1 for now */
+  };
+
+  return factory_new (tiz_get_type (ap_hdl, "tizbinaryport"),
+                      &port_opts);
+}
+
+static OMX_PTR
+instantiate_config_port (OMX_HANDLETYPE ap_hdl)
+{
+  /* Instantiate the config port */
+  return factory_new (tiz_get_type (ap_hdl, "tizuricfgport"),
+                      NULL,       /* this port does not take options */
+                      ARATELIA_INPROC_READER_COMPONENT_NAME,
+                      inproc_reader_version);
+}
+
+static OMX_PTR
+instantiate_processor (OMX_HANDLETYPE ap_hdl)
+{
+  return factory_new (tiz_get_type (ap_hdl, "inprocsrc_prc"));
+}
+
+OMX_ERRORTYPE
+OMX_ComponentInit (OMX_HANDLETYPE ap_hdl)
+{
+  tiz_role_factory_t audio_role;
+  tiz_role_factory_t video_role;
+  tiz_role_factory_t image_role;
+  tiz_role_factory_t other_role;
+  const tiz_role_factory_t *rf_list[] = { &audio_role, &video_role,
+    &image_role, &other_role
+  };
+  tiz_type_factory_t inprocsrc_prc_type;
+  const tiz_type_factory_t *tf_list[] = { &inprocsrc_prc_type };
+
+  strcpy ((OMX_STRING) audio_role.role,
+          ARATELIA_INPROC_READER_AUDIO_ROLE);
+  audio_role.pf_cport   = instantiate_config_port;
+  audio_role.pf_port[0] = instantiate_audio_port;
+  audio_role.nports     = 1;
+  audio_role.pf_proc    = instantiate_processor;
+
+  strcpy ((OMX_STRING) video_role.role,
+          ARATELIA_INPROC_READER_VIDEO_ROLE);
+  video_role.pf_cport   = instantiate_config_port;
+  video_role.pf_port[0] = instantiate_video_port;
+  video_role.nports     = 1;
+  video_role.pf_proc    = instantiate_processor;
+
+  strcpy ((OMX_STRING) image_role.role,
+          ARATELIA_INPROC_READER_IMAGE_ROLE);
+  image_role.pf_cport   = instantiate_config_port;
+  image_role.pf_port[0] = instantiate_image_port;
+  image_role.nports     = 1;
+  image_role.pf_proc    = instantiate_processor;
+
+  strcpy ((OMX_STRING) other_role.role,
+          ARATELIA_INPROC_READER_OTHER_ROLE);
+  other_role.pf_cport   = instantiate_config_port;
+  other_role.pf_port[0] = instantiate_other_port;
+  other_role.nports     = 1;
+  other_role.pf_proc    = instantiate_processor;
+
+  strcpy ((OMX_STRING) inprocsrc_prc_type.class_name, "inprocsrc_prc_class");
+  inprocsrc_prc_type.pf_class_init = inprocsrc_prc_class_init;
+  strcpy ((OMX_STRING) inprocsrc_prc_type.object_name, "inprocsrc_prc");
+  inprocsrc_prc_type.pf_object_init = inprocsrc_prc_init;
+
+  /* Initialize the component infrastructure */
+  tiz_check_omx_err (tiz_comp_init (ap_hdl, ARATELIA_INPROC_READER_COMPONENT_NAME));
+
+  /* Register the "inprocsrc_prc" class */
+  tiz_check_omx_err (tiz_comp_register_types (ap_hdl, tf_list, 1));
+
+  /* Register the various roles */
+  tiz_check_omx_err (tiz_comp_register_roles (ap_hdl, rf_list, 4));
+
+  return OMX_ErrorNone;
+}
---
+++ plugins/inproc_reader/src/inprocsrcprc.c
@@ -0,0 +1,179 @@
+/**
+ * Copyright (C) 2011-2015 Aratelia Limited - Juan A. Rubio
+ *
+ * This file is part of Tizonia
+ *
+ * Tizonia is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Tizonia is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Tizonia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file   inprocsrcprc.c
+ * @author Juan A. Rubio <juan.rubio@aratelia.com>
+ *
+ * @brief  Tizonia OpenMAX IL - ZMQ inproc socket reader
+ *
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <assert.h>
+
+#include <tizplatform.h>
+
+#include <tizkernel.h>
+#include <tizscheduler.h>
+
+#include "inprocsrc.h"
+#include "inprocsrcprc.h"
+#include "inprocsrcprc_decls.h"
+
+#ifdef TIZ_LOG_CATEGORY_NAME
+#undef TIZ_LOG_CATEGORY_NAME
+#define TIZ_LOG_CATEGORY_NAME "tiz.inproc_reader.prc"
+#endif
+
+/*
+ * inprocsrcprc
+ */
+
+static void *
+inprocsrc_prc_ctor (void *ap_obj, va_list * app)
+{
+  inprocsrc_prc_t *p_obj = super_ctor (typeOf (ap_obj, "inprocsrcprc"), ap_obj, app);
+  p_obj->eos_ = false;
+  return p_obj;
+}
+
+static void *
+inprocsrc_prc_dtor (void *ap_obj)
+{
+  return super_dtor (typeOf (ap_obj, "inprocsrcprc"), ap_obj);
+}
+
+static OMX_ERRORTYPE
+inprocsrc_prc_read_buffer (const void *ap_obj, OMX_BUFFERHEADERTYPE * p_hdr)
+{
+  return OMX_ErrorNone;
+}
+
+/*
+ * from tizsrv class
+ */
+
+static OMX_ERRORTYPE
+inprocsrc_prc_allocate_resources (void *ap_obj, OMX_U32 a_pid)
+{
+  return OMX_ErrorNone;
+}
+
+static OMX_ERRORTYPE
+inprocsrc_prc_deallocate_resources (void *ap_obj)
+{
+  return OMX_ErrorNone;
+}
+
+static OMX_ERRORTYPE
+inprocsrc_prc_prepare_to_transfer (void *ap_obj, OMX_U32 a_pid)
+{
+  return OMX_ErrorNone;
+}
+
+static OMX_ERRORTYPE
+inprocsrc_prc_transfer_and_process (void *ap_obj, OMX_U32 a_pid)
+{
+  return OMX_ErrorNone;
+}
+
+static OMX_ERRORTYPE
+inprocsrc_prc_stop_and_return (void *ap_obj)
+{
+  return OMX_ErrorNone;
+}
+
+/*
+ * from tizprc class
+ */
+
+static OMX_ERRORTYPE
+inprocsrc_prc_buffers_ready (const void *ap_obj)
+{
+  return OMX_ErrorNone;
+}
+
+/*
+ * inprocsrc_prc_class
+ */
+
+static void *
+inprocsrc_prc_class_ctor (void *ap_obj, va_list * app)
+{
+  /* NOTE: Class methods might be added in the future. None for now. */
+  return super_ctor (typeOf (ap_obj, "inprocsrcprc_class"), ap_obj, app);
+}
+
+/*
+ * initialization
+ */
+
+void *
+inprocsrc_prc_class_init (void * ap_tos, void * ap_hdl)
+{
+  void * tizprc = tiz_get_type (ap_hdl, "tizprc");
+  void * inprocsrcprc_class = factory_new
+    /* TIZ_CLASS_COMMENT: class type, class name, parent, size */
+    (classOf (tizprc), "inprocsrcprc_class", classOf (tizprc), sizeof (inprocsrc_prc_class_t),
+     /* TIZ_CLASS_COMMENT: */
+     ap_tos, ap_hdl,
+     /* TIZ_CLASS_COMMENT: class constructor */
+     ctor, inprocsrc_prc_class_ctor,
+     /* TIZ_CLASS_COMMENT: stop value*/
+     0);
+  return inprocsrcprc_class;
+}
+
+void *
+inprocsrc_prc_init (void * ap_tos, void * ap_hdl)
+{
+  void * tizprc = tiz_get_type (ap_hdl, "tizprc");
+  void * inprocsrcprc_class = tiz_get_type (ap_hdl, "inprocsrcprc_class");
+  TIZ_LOG_CLASS (inprocsrcprc_class);
+  void * inprocsrcprc = factory_new
+    /* TIZ_CLASS_COMMENT: class type, class name, parent, size */
+    (inprocsrcprc_class, "inprocsrcprc", tizprc, sizeof (inprocsrc_prc_t),
+     /* TIZ_CLASS_COMMENT: */
+     ap_tos, ap_hdl,
+     /* TIZ_CLASS_COMMENT: class constructor */
+     ctor, inprocsrc_prc_ctor,
+     /* TIZ_CLASS_COMMENT: class destructor */
+     dtor, inprocsrc_prc_dtor,
+     /* TIZ_CLASS_COMMENT: */
+     tiz_srv_allocate_resources, inprocsrc_prc_allocate_resources,
+     /* TIZ_CLASS_COMMENT: */
+     tiz_srv_deallocate_resources, inprocsrc_prc_deallocate_resources,
+     /* TIZ_CLASS_COMMENT: */
+     tiz_srv_prepare_to_transfer, inprocsrc_prc_prepare_to_transfer,
+     /* TIZ_CLASS_COMMENT: */
+     tiz_srv_transfer_and_process, inprocsrc_prc_transfer_and_process,
+     /* TIZ_CLASS_COMMENT: */
+     tiz_srv_stop_and_return, inprocsrc_prc_stop_and_return,
+     /* TIZ_CLASS_COMMENT: */
+     tiz_prc_buffers_ready, inprocsrc_prc_buffers_ready,
+     /* TIZ_CLASS_COMMENT: stop value */
+     0);
+
+  return inprocsrcprc;
+}
