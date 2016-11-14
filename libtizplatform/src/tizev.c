fb5cfd6e3ea4ceba2efe919e6b0ee6efff36bada
Juan A. Rubio <jarubio2001@gmail.com>
2016-11-14 12:45:42 +0000

    libtizplatform: fixed Coverity Scan (CID 1352353) ([/libtizplatform/src/tizev.c:enqueue_timer_msg] : Missing unlock #219) (closes #219)

--- libtizplatform/src/tizev.c
+++ libtizplatform/src/tizev.c
@@ -355,6 +355,7 @@ static OMX_ERRORTYPE
 enqueue_timer_msg (tiz_event_timer_t * ap_ev_timer, const uint32_t a_id,
                    const tiz_event_loop_msg_class_t a_class)
 {
+  OMX_ERRORTYPE rc = OMX_ErrorUndefined;
   tiz_event_loop_msg_t * p_msg = NULL;
   tiz_event_loop_msg_timer_t * p_msg_timer = NULL;
 
@@ -365,18 +366,30 @@ enqueue_timer_msg (tiz_event_timer_t * ap_ev_timer, const uint32_t a_id,
           || ETIZEventLoopMsgTimerDestroy == a_class);
 
   tiz_check_omx (tiz_mutex_lock (&(gp_event_loop->mutex)));
-  p_msg = init_event_loop_msg (gp_event_loop, (a_class));
-  tiz_check_null_ret_oom (p_msg != NULL);
+  tiz_goto_end_on_null ((p_msg = init_event_loop_msg (gp_event_loop, (a_class))),
+                        "Failed to initialise the event loop");
 
   assert (p_msg);
   p_msg_timer = &(p_msg->timer);
   p_msg_timer->p_ev_timer = ap_ev_timer;
   p_msg_timer->id = a_id;
-  tiz_pqueue_send (gp_event_loop->p_pq, p_msg, p_msg->priority);
+  tiz_goto_end_on_omx_err (
+    tiz_pqueue_send (gp_event_loop->p_pq, p_msg, p_msg->priority),
+    "Failed to insert into the queue");
   tiz_check_omx (tiz_mutex_unlock (&(gp_event_loop->mutex)));
   ev_async_send (gp_event_loop->p_loop, gp_event_loop->p_async_watcher);
 
-  return OMX_ErrorNone;
+  /* All good */
+  rc = OMX_ErrorNone;
+
+ end:
+
+    if (OMX_ErrorNone != rc)
+    {
+      tiz_check_omx (tiz_mutex_unlock (&(gp_event_loop->mutex)));
+    }
+
+  return rc;
 }
 
 static OMX_ERRORTYPE
