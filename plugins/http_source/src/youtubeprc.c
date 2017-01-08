6c572976ba5cfae091c642cf9f3b01b4cf2433b5
Juan A. Rubio <juan.rubio@aratelia.com>
2017-01-08 23:10:26 +0000

    http_source: youtube - make use of 'OMX_AUDIO_YoutubePlaylistTypeAudioMix'

--- plugins/http_source/src/youtubeprc.c
+++ plugins/http_source/src/youtubeprc.c
@@ -682,6 +682,11 @@ enqueue_playlist_items (youtube_prc_t * ap_prc)
             rc = tiz_youtube_play_audio_playlist (ap_prc->p_youtube_, p_playlist);
           }
           break;
+        case OMX_AUDIO_YoutubePlaylistTypeAudioMix:
+          {
+            rc = tiz_youtube_play_audio_mix (ap_prc->p_youtube_, p_playlist);
+          }
+          break;
         case OMX_AUDIO_YoutubePlaylistTypeAudioSearch:
           {
             rc = tiz_youtube_play_audio_search (ap_prc->p_youtube_, p_playlist);
