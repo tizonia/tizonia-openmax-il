3c26d843586e3185877555759c8d0c170bfccfdb
Juan A. Rubio <jarubio2001@gmail.com>
2017-01-13 16:54:20 +0000

    spotify_source: fixed playback not happening if there are playlists that never get "ready"

--- plugins/spotify_source/src/spfysrcprc.c
+++ plugins/spotify_source/src/spfysrcprc.c
@@ -100,14 +100,21 @@ struct spfy_music_delivery_data
 static OMX_S32
 ready_playlist_map_compare_func (OMX_PTR ap_key1, OMX_PTR ap_key2)
 {
-  return strncmp ((const char *) ap_key1, (const char *) ap_key2,
-                  SPFYSRC_MAX_STRING_SIZE);
+  if (ap_key1 < ap_key2)
+    {
+      return -1;
+    }
+  else if (ap_key1 > ap_key2)
+    {
+      return 1;
+    }
+  return 0;
 }
 
 static void
 ready_playlist_map_free_func (OMX_PTR ap_key, OMX_PTR ap_value)
 {
-  tiz_mem_free (ap_key);
+  /* Nothing to delete */
 }
 
 static OMX_S32
@@ -127,7 +134,7 @@ not_ready_playlist_map_compare_func (OMX_PTR ap_key1, OMX_PTR ap_key2)
 static void
 not_ready_playlist_map_free_func (OMX_PTR ap_key, OMX_PTR ap_value)
 {
-/*   tiz_mem_free (ap_key); */
+  /* Nothing to delete */
 }
 
 static unsigned int
@@ -184,32 +191,34 @@ get_cache_location (char * user)
 static bool
 playlist_name_partial_match (spfysrc_prc_t * ap_prc, const char * ap_pl_name)
 {
+  bool outcome = false;
   assert (ap_prc);
   assert (ap_pl_name);
 
   if (strcasestr (ap_pl_name, (const char *) ap_prc->playlist_.cPlaylistName))
     {
-      TIZ_DEBUG (handleOf (ap_prc), "PARTIAL MATCH [%s] vs [%s]", ap_pl_name,
-                 ap_prc->playlist_.cPlaylistName);
-      return true;
+      outcome = true;
     }
-  return false;
+  TIZ_DEBUG (handleOf (ap_prc), "PARTIAL MATCH [%s] vs [%s] ? [%s]", ap_pl_name,
+             ap_prc->playlist_.cPlaylistName, (outcome ? "YES" : "NO"));
+  return outcome;
 }
 
 static bool
 playlist_name_exact_match (spfysrc_prc_t * ap_prc, const char * ap_pl_name)
 {
+  bool outcome = false;
   assert (ap_prc);
   assert (ap_pl_name);
 
   if (!strncasecmp (ap_pl_name, (const char *) ap_prc->playlist_.cPlaylistName,
                     SPFYSRC_MAX_STRING_SIZE))
     {
-      TIZ_DEBUG (handleOf (ap_prc), "EXACT MATCH [%s] vs [%s]", ap_pl_name,
-                 ap_prc->playlist_.cPlaylistName);
-      return true;
+      outcome = true;
     }
-  return false;
+  TIZ_DEBUG (handleOf (ap_prc), "EXACT MATCH [%s] vs [%s] ? [%s]", ap_pl_name,
+             ap_prc->playlist_.cPlaylistName, (outcome ? "YES" : "NO"));
+  return outcome;
 }
 
 static bool
@@ -226,6 +235,8 @@ playlist_name_match (spfysrc_prc_t * ap_prc, const char * ap_pl_name,
     {
       outcome = playlist_name_partial_match (ap_prc, ap_pl_name);
     }
+  TIZ_DEBUG (handleOf (ap_prc), "MATCH [%s] ? [%s]", ap_pl_name,
+             (outcome ? "YES" : "NO"));
   return outcome;
 }
 
@@ -1355,6 +1366,9 @@ playlist_removed (sp_playlistcontainer * pc, sp_playlist * pl, int position,
   sp_playlist_remove_callbacks (pl, &(p_prc->sp_pl_cbacks_), NULL);
   TIZ_PRINTF_YEL ("[Spotify] : playlist removed [%s] - position [%d]\n",
                   sp_playlist_name (pl), position);
+  tiz_map_erase (p_prc->p_not_ready_playlists_, pl);
+  tiz_map_erase (p_prc->p_ready_playlists_, pl);
+  p_prc->nplaylists_ = sp_playlistcontainer_num_playlists (pc);
 }
 
 /**
@@ -1501,21 +1515,19 @@ static void
 playlist_state_changed (sp_playlist * pl, void * userdata)
 {
   spfysrc_prc_t * p_prc = userdata;
+  bool exact_match = false;
   assert (p_prc);
 
   if (sp_playlist_is_loaded (pl) && !sp_playlist_has_pending_changes (pl))
     {
       OMX_ERRORTYPE rc = OMX_ErrorNone;
       spfysrc_prc_t * p_prc = userdata;
-      bool exact_match = false;
       OMX_U32 ready_playlists_count
         = (OMX_U32) tiz_map_size (p_prc->p_ready_playlists_);
 
       /* Record that this playlist is ready for playback */
-      rc = tiz_map_insert (
-        p_prc->p_ready_playlists_,
-        strndup (sp_playlist_name (pl), SPFYSRC_MAX_STRING_SIZE), pl,
-        (OMX_U32 *) (&ready_playlists_count));
+      rc = tiz_map_insert (p_prc->p_ready_playlists_, pl, pl,
+                           (OMX_U32 *) (&ready_playlists_count));
 
       /* Make sure this playlist is not found in the "not ready" list */
       tiz_map_erase (p_prc->p_not_ready_playlists_, pl);
@@ -1527,41 +1539,52 @@ playlist_state_changed (sp_playlist * pl, void * userdata)
                           tiz_map_size (p_prc->p_ready_playlists_),
                           p_prc->nplaylists_, sp_playlist_num_tracks (pl));
         }
-
-      if (playlist_name_match (p_prc, sp_playlist_name (pl), &exact_match))
-        {
-          if (exact_match)
-            {
-              p_prc->p_sp_playlist_ = pl;
-            }
-          else if (!p_prc->p_sp_tentative_playlist_)
-            {
-              p_prc->p_sp_tentative_playlist_ = pl;
-            }
-        }
     }
   else
     {
-      /* This is a bit of a hack. When playlists start becoming read for playback (i.e. loaded and no pending changes) */
+      /* This is a bit of a hack. When playlists start becoming ready for
+         playback (i.e. loaded and no pending changes) */
       /* we'll start tracking those playlists that are 'not ready'. We'll use
          the counts of both type of playlists, to figure out when spotify has
          completed the metadata update process */
-      if (tiz_map_size (p_prc->p_ready_playlists_) > 0
-          && 0 == sp_playlist_num_tracks (pl))
-        {
-          OMX_U32 not_ready_playlists_count
-            = (OMX_U32) tiz_map_size (p_prc->p_not_ready_playlists_);
+      /*       if (tiz_map_size (p_prc->p_ready_playlists_) > 0 */
+      /*           && 0 == sp_playlist_num_tracks (pl)) */
+      {
+        OMX_U32 not_ready_playlists_count
+          = (OMX_U32) tiz_map_size (p_prc->p_not_ready_playlists_);
 
-          /* Record that this playlist is not_ready for playback for whatever reason */
-          (void) tiz_map_insert (p_prc->p_not_ready_playlists_, pl, pl,
-                                 (OMX_U32 *) (&not_ready_playlists_count));
-          TIZ_DEBUG (
-            handleOf (p_prc), "NON-READY PLAYLIST [%p] [%s] [%s]", pl,
-            (sp_playlist_is_loaded (pl) ? "LOADED" : "NOT LOADED"),
-            (sp_playlist_has_pending_changes (pl) ? "PENDING CHANGEs"
-                                                  : "NO PENDING CHANGES"));
+        /* Record that this playlist is not_ready for playback for whatever reason */
+        (void) tiz_map_insert (p_prc->p_not_ready_playlists_, pl, pl,
+                               (OMX_U32 *) (&not_ready_playlists_count));
+
+        /* Make sure this playlist is not found in the "ready" list */
+        tiz_map_erase (p_prc->p_ready_playlists_, pl);
+
+        TIZ_DEBUG (
+          handleOf (p_prc), "NON-READY PLAYLIST [%p] [%s] [%s] [%s]", pl,
+          sp_playlist_name (pl),
+          (sp_playlist_is_loaded (pl) ? "LOADED" : "NOT LOADED"),
+          (sp_playlist_has_pending_changes (pl) ? "PENDING CHANGEs"
+                                                : "NO PENDING CHANGES"));
+      }
+    }
+
+  if (playlist_name_match (p_prc, sp_playlist_name (pl), &exact_match))
+    {
+      if (exact_match)
+        {
+          TIZ_DEBUG (handleOf (p_prc), "Found exact match : %s",
+                     sp_playlist_name (pl));
+          p_prc->p_sp_playlist_ = pl;
+        }
+      else if (!p_prc->p_sp_tentative_playlist_)
+        {
+          TIZ_DEBUG (handleOf (p_prc), "Found partial match : %s",
+                     sp_playlist_name (pl));
+          p_prc->p_sp_tentative_playlist_ = pl;
         }
     }
+
   TIZ_DEBUG (handleOf (p_prc), "playlists count [%d] [%d] [%d]",
              p_prc->nplaylists_, tiz_map_size (p_prc->p_ready_playlists_),
              tiz_map_size (p_prc->p_not_ready_playlists_));
@@ -1571,11 +1594,13 @@ playlist_state_changed (sp_playlist * pl, void * userdata)
   /*   b) the playlist metadata update process has finished, and the ready and */
   /*      not-ready playlist count equals the total number of playlists in the */
   /*      container */
-  if (p_prc->p_sp_playlist_
-      || ((0 != p_prc->nplaylists_
-           && p_prc->nplaylists_
-                == (tiz_map_size (p_prc->p_ready_playlists_)
-                    + tiz_map_size (p_prc->p_not_ready_playlists_)))))
+  if (!p_prc->spotify_inited_
+      && (p_prc->p_sp_playlist_
+          || ((0 != p_prc->nplaylists_
+               && (tiz_map_size (p_prc->p_ready_playlists_) > 0)
+               && p_prc->nplaylists_
+                    == (tiz_map_size (p_prc->p_ready_playlists_)
+                        + tiz_map_size (p_prc->p_not_ready_playlists_))))))
     {
       TIZ_DEBUG (handleOf (p_prc), "p_sp_playlist [%p] tentative [%p]",
                  p_prc->p_sp_playlist_, p_prc->p_sp_tentative_playlist_);
