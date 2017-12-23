ad539966647e861bbbb62a82c59c2bf8ed9e13fe
Juan A. Rubio <jarubio2001@gmail.com>
2017-12-23 21:47:38 +0000

    tizonia-snap-wrapper.sh: allow armhf and i386

--- snap/wrapper/tizonia-snap-wrapper.sh
+++ snap/wrapper/tizonia-snap-wrapper.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 case "$SNAP_ARCH" in
-	"amd64") ARCH='x86_64-linux-gnu'
-	;;
-	*)
-		echo "Unsupported architecture for this app build"
-		exit 1
-	;;
+    "amd64") ARCH='x86_64-linux-gnu'
+             ;;
+    "i386") ARCH='i386-linux-gnu'
+            ;;
+    "armhf") ARCH="arm-linux-gnueabihf"
+             ;;
+    *)
+        echo "Unsupported architecture for this app build"
+        exit 1
+        ;;
 esac
 
 # XDG Config
