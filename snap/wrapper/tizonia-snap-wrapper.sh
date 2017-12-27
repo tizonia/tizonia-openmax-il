10c1502ebdeaee7b5ad9e1841d4662e6c41e0bff
Juan A. Rubio <jarubio2001@gmail.com>
2017-12-27 10:55:37 +0000

    Revert "snap/snapcraft.yaml: removed 'x11' plug; added pulseaudio exports in wrapper script"
    This reverts commit 5e2b12d20f20fafc0b619ca89d5da20db8e4cac7.

--- snap/wrapper/tizonia-snap-wrapper.sh
+++ snap/wrapper/tizonia-snap-wrapper.sh
@@ -13,22 +13,6 @@ case "$SNAP_ARCH" in
         ;;
 esac
 
-# Pulseaudio export
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SNAP/usr/lib/$ARCH/pulseaudio
-
-# Make PulseAudio socket available inside the snap-specific $XDG_RUNTIME_DIR
-if [ -n "$XDG_RUNTIME_DIR" ]; then
-    pulsenative="pulse/native"
-    pulseaudio_sockpath="$XDG_RUNTIME_DIR/../$pulsenative"
-    pulseaudio_snappath="$XDG_RUNTIME_DIR/$pulsenative"
-    if [ -S "$pulseaudio_sockpath" ]; then
-        if [ ! -e "$pulseaudio_snappath" ]; then
-            mkdir -p -m 700 $(dirname "$pulseaudio_snappath")
-            ln -s "$pulseaudio_sockpath" "$pulseaudio_snappath"
-        fi
-    fi
-fi
-
 # XDG Config
 export XDG_CONFIG_DIRS=$SNAP/etc/xdg:$XDG_CONFIG_DIRS
 
