08d2da04d5d04f820e806ac972d0ef8832b39ffc
Juan A. Rubio <jarubio2001@gmail.com>
2018-02-05 23:07:49 +0000

    chromecast_renderer: cc_scloudprc now inherits cc_prc

--- plugins/chromecast_renderer/src/cc_scloudprc.c
+++ plugins/chromecast_renderer/src/cc_scloudprc.c
@@ -52,16 +52,8 @@
 #endif
 
 /* forward declarations */
-static OMX_ERRORTYPE
-cc_scloud_prc_deallocate_resources (void *);
-static OMX_ERRORTYPE
-release_buffer (cc_scloud_prc_t *);
-static OMX_ERRORTYPE
-cc_scloud_prc_prepare_to_transfer (void * ap_prc, OMX_U32 a_pid);
-static OMX_ERRORTYPE
-cc_scloud_prc_transfer_and_process (void * ap_prc, OMX_U32 a_pid);
 
-#define on_lib_error_ret_omx_oom(expr)                                    \
+#define on_scloud_error_ret_omx_oom(expr)                                    \
   do                                                                         \
     {                                                                        \
       int scloud_error = 0;                                                  \
@@ -76,199 +68,7 @@ cc_scloud_prc_transfer_and_process (void * ap_prc, OMX_U32 a_pid);
   while (0)
 
 static OMX_ERRORTYPE
-store_metadata (cc_scloud_prc_t * ap_prc, const char * ap_header_name,
-                const char * ap_header_info)
-{
-  OMX_ERRORTYPE rc = OMX_ErrorNone;
-  OMX_CONFIG_METADATAITEMTYPE * p_meta = NULL;
-  size_t metadata_len = 0;
-  size_t info_len = 0;
-
-  assert (ap_prc);
-  if (ap_header_name && ap_header_info)
-    {
-      info_len = strnlen (ap_header_info, OMX_MAX_STRINGNAME_SIZE - 1) + 1;
-      metadata_len = sizeof (OMX_CONFIG_METADATAITEMTYPE) + info_len;
-
-      if (NULL == (p_meta = (OMX_CONFIG_METADATAITEMTYPE *) tiz_mem_calloc (
-                     1, metadata_len)))
-        {
-          rc = OMX_ErrorInsufficientResources;
-        }
-      else
-        {
-          const size_t name_len
-            = strnlen (ap_header_name, OMX_MAX_STRINGNAME_SIZE - 1) + 1;
-          strncpy ((char *) p_meta->nKey, ap_header_name, name_len - 1);
-          p_meta->nKey[name_len - 1] = '\0';
-          p_meta->nKeySizeUsed = name_len;
-
-          strncpy ((char *) p_meta->nValue, ap_header_info, info_len - 1);
-          p_meta->nValue[info_len - 1] = '\0';
-          p_meta->nValueMaxSize = info_len;
-          p_meta->nValueSizeUsed = info_len;
-
-          p_meta->nSize = metadata_len;
-          p_meta->nVersion.nVersion = OMX_VERSION;
-          p_meta->eScopeMode = OMX_MetadataScopeAllLevels;
-          p_meta->nScopeSpecifier = 0;
-          p_meta->nMetadataItemIndex = 0;
-          p_meta->eSearchMode = OMX_MetadataSearchValueSizeByIndex;
-          p_meta->eKeyCharset = OMX_MetadataCharsetASCII;
-          p_meta->eValueCharset = OMX_MetadataCharsetASCII;
-
-          rc = tiz_krn_store_metadata (tiz_get_krn (handleOf (ap_prc)), p_meta);
-        }
-    }
-  return rc;
-}
-
-static inline void
-delete_uri (cc_scloud_prc_t * ap_prc)
-{
-  assert (ap_prc);
-  tiz_mem_free (ap_prc->p_uri_param_);
-  ap_prc->p_uri_param_ = NULL;
-}
-
-static OMX_ERRORTYPE
-update_metadata (cc_scloud_prc_t * ap_prc)
-{
-  assert (ap_prc);
-
-  /* Clear previous metatada items */
-  tiz_krn_clear_metadata (tiz_get_krn (handleOf (ap_prc)));
-
-  /* User and track title */
-  tiz_check_omx (store_metadata (
-    ap_prc, tiz_scloud_get_current_track_user (ap_prc->p_sc_),
-    tiz_scloud_get_current_track_title (ap_prc->p_sc_)));
-
-  /* Store the year if not 0 */
-  {
-    const char * p_year = tiz_scloud_get_current_track_year (ap_prc->p_sc_);
-    if (p_year && strncmp (p_year, "0", 4) != 0)
-      {
-        tiz_check_omx (store_metadata (ap_prc, "Year", p_year));
-      }
-  }
-
-  /* Duration */
-  tiz_check_omx (
-    store_metadata (ap_prc, "Duration",
-                    tiz_scloud_get_current_track_duration (ap_prc->p_sc_)));
-
-  /* Likes */
-  tiz_check_omx (
-    store_metadata (ap_prc, "Likes count",
-                    tiz_scloud_get_current_track_likes (ap_prc->p_sc_)));
-
-  /* Permalink */
-  tiz_check_omx (store_metadata (
-    ap_prc, "Permalink",
-    tiz_scloud_get_current_track_permalink (ap_prc->p_sc_)));
-
-  /* License */
-  tiz_check_omx (
-    store_metadata (ap_prc, "License",
-                    tiz_scloud_get_current_track_license (ap_prc->p_sc_)));
-
-  /* Signal that a new set of metatadata items is available */
-  (void) tiz_srv_issue_event ((OMX_PTR) ap_prc, OMX_EventIndexSettingChanged,
-                              OMX_ALL, /* no particular port associated */
-                              OMX_IndexConfigMetadataItem, /* index of the
-                                                             struct that has
-                                                             been modififed */
-                              NULL);
-
-  return OMX_ErrorNone;
-}
-
-static OMX_ERRORTYPE
-obtain_next_url (cc_scloud_prc_t * ap_prc, int a_skip_value)
-{
-  OMX_ERRORTYPE rc = OMX_ErrorNone;
-  const long pathname_max = PATH_MAX + NAME_MAX;
-
-  assert (ap_prc);
-  assert (ap_prc->p_sc_);
-
-  if (!ap_prc->p_uri_param_)
-    {
-      ap_prc->p_uri_param_ = tiz_mem_calloc (
-        1, sizeof (OMX_PARAM_CONTENTURITYPE) + pathname_max + 1);
-    }
-
-  tiz_check_null_ret_oom (ap_prc->p_uri_param_);
-
-  ap_prc->p_uri_param_->nSize
-    = sizeof (OMX_PARAM_CONTENTURITYPE) + pathname_max + 1;
-  ap_prc->p_uri_param_->nVersion.nVersion = OMX_VERSION;
-
-  {
-    const char * p_next_url = a_skip_value > 0
-                                ? tiz_scloud_get_next_url (ap_prc->p_sc_)
-                                : tiz_scloud_get_prev_url (ap_prc->p_sc_);
-    tiz_check_null_ret_oom (p_next_url);
-
-    {
-      const OMX_U32 url_len = strnlen (p_next_url, pathname_max);
-      TIZ_TRACE (handleOf (ap_prc), "URL [%s]", p_next_url);
-
-      /* Verify we are getting an chromecast scheme */
-      if (!p_next_url || !url_len
-          || (memcmp (p_next_url, "chromecast://", 7) != 0
-              && memcmp (p_next_url, "chromecasts://", 8) != 0))
-        {
-          rc = OMX_ErrorContentURIError;
-        }
-      else
-        {
-          strncpy ((char *) ap_prc->p_uri_param_->contentURI, p_next_url,
-                   url_len);
-          ap_prc->p_uri_param_->contentURI[url_len] = '\000';
-
-          /* Song metadata is now available, update the IL client */
-          rc = update_metadata (ap_prc);
-        }
-    }
-  }
-
-  return rc;
-}
-
-static OMX_ERRORTYPE
-release_buffer (cc_scloud_prc_t * ap_prc)
-{
-  assert (ap_prc);
-
-  if (ap_prc->p_inhdr_)
-    {
-      if (ap_prc->bytes_before_eos_ > ap_prc->p_inhdr_->nFilledLen)
-        {
-          ap_prc->bytes_before_eos_ -= ap_prc->p_inhdr_->nFilledLen;
-        }
-      else
-        {
-          ap_prc->bytes_before_eos_ = 0;
-          ap_prc->eos_ = true;
-        }
-
-      if (ap_prc->eos_)
-        {
-          ap_prc->eos_ = false;
-          ap_prc->p_inhdr_->nFlags |= OMX_BUFFERFLAG_EOS;
-        }
-      tiz_check_omx (tiz_krn_release_buffer (
-        tiz_get_krn (handleOf (ap_prc)),
-        ARATELIA_CHROMECAST_RENDERER_PORT_INDEX, ap_prc->p_inhdr_));
-      ap_prc->p_inhdr_ = NULL;
-    }
-  return OMX_ErrorNone;
-}
-
-static OMX_ERRORTYPE
-retrieve_session_configuration (cc_scloud_prc_t * ap_prc)
+retrieve_sc_session (cc_scloud_prc_t * ap_prc)
 {
   return tiz_api_GetParameter (
     tiz_get_krn (handleOf (ap_prc)), handleOf (ap_prc),
@@ -276,15 +76,15 @@ retrieve_session_configuration (cc_scloud_prc_t * ap_prc)
 }
 
 static OMX_ERRORTYPE
-retrieve_playlist (cc_scloud_prc_t * ap_prc)
+retrieve_sc_playlist (cc_scloud_prc_t * ap_prc)
 {
   return tiz_api_GetParameter (
     tiz_get_krn (handleOf (ap_prc)), handleOf (ap_prc),
-    OMX_TizoniaIndexParamAudioSoundCloudPlaylist, &(ap_prc->playlist_));
+    OMX_TizoniaIndexParamAudioSoundCloudPlaylist, &(ap_prc->sc_playlist_));
 }
 
 static OMX_ERRORTYPE
-enqueue_playlist_items (cc_scloud_prc_t * ap_prc)
+enqueue_gm_playlist_items (cc_scloud_prc_t * ap_prc)
 {
   int rc = 1;
 
@@ -292,14 +92,14 @@ enqueue_playlist_items (cc_scloud_prc_t * ap_prc)
   assert (ap_prc->p_sc_);
 
   {
-    const char * p_playlist = (const char *) ap_prc->playlist_.cPlaylistName;
-    const OMX_BOOL shuffle = ap_prc->playlist_.bShuffle;
+    const char * p_playlist = (const char *) ap_prc->sc_playlist_.cPlaylistName;
+    const OMX_BOOL shuffle = ap_prc->sc_playlist_.bShuffle;
 
     tiz_scloud_set_playback_mode (
       ap_prc->p_sc_, (shuffle == OMX_TRUE ? ETIZScloudPlaybackModeShuffle
                                               : ETIZScloudPlaybackModeNormal));
 
-    switch (ap_prc->playlist_.ePlaylistType)
+    switch (ap_prc->sc_playlist_.ePlaylistType)
       {
         case OMX_AUDIO_SoundCloudPlaylistTypeUnknown:
           {
@@ -367,24 +167,14 @@ cc_scloud_prc_ctor (void * ap_obj, va_list * app)
   cc_scloud_prc_t * p_prc
     = super_ctor (typeOf (ap_obj, "cc_scloudprc"), ap_obj, app);
   TIZ_INIT_OMX_STRUCT (p_prc->sc_session_);
-  TIZ_INIT_OMX_STRUCT (p_prc->playlist_);
-  TIZ_INIT_OMX_STRUCT (p_prc->playlist_skip_);
-  TIZ_INIT_OMX_STRUCT (p_prc->cc_session_);
-  p_prc->p_uri_param_ = NULL;
-  p_prc->p_inhdr_ = NULL;
+  TIZ_INIT_OMX_STRUCT (p_prc->sc_playlist_);
   p_prc->p_sc_ = NULL;
-  p_prc->p_cc_ = NULL;
-  p_prc->eos_ = false;
-  p_prc->port_disabled_ = false;
-  p_prc->uri_changed_ = false;
-  p_prc->bytes_before_eos_ = 0;
   return p_prc;
 }
 
 static void *
 cc_scloud_prc_dtor (void * ap_obj)
 {
-  (void) cc_scloud_prc_deallocate_resources (ap_obj);
   return super_dtor (typeOf (ap_obj, "cc_scloudprc"), ap_obj);
 }
 
@@ -396,18 +186,20 @@ static OMX_ERRORTYPE
 cc_scloud_prc_allocate_resources (void * ap_obj, OMX_U32 a_pid)
 {
   cc_scloud_prc_t * p_prc = ap_obj;
-  OMX_ERRORTYPE rc = OMX_ErrorInsufficientResources;
   assert (p_prc);
-  tiz_check_omx (retrieve_session_configuration (p_prc));
-  tiz_check_omx (retrieve_playlist (p_prc));
 
-  on_lib_error_ret_omx_oom (tiz_scloud_init (
+  tiz_check_omx (tiz_srv_super_allocate_resources (
+    typeOf (p_prc, "cc_scloudprc"), p_prc, a_pid));
+
+  tiz_check_omx (retrieve_sc_session (p_prc));
+  tiz_check_omx (retrieve_sc_playlist (p_prc));
+
+  on_scloud_error_ret_omx_oom (tiz_scloud_init (
     &(p_prc->p_sc_), (const char *) p_prc->sc_session_.cUserOauthToken));
 
-  tiz_check_omx (enqueue_playlist_items (p_prc));
-  tiz_check_omx (obtain_next_url (p_prc, 1));
+  tiz_check_omx (enqueue_gm_playlist_items (p_prc));
 
-  return rc;
+  return OMX_ErrorNone;
 }
 
 static OMX_ERRORTYPE
@@ -415,132 +207,82 @@ cc_scloud_prc_deallocate_resources (void * ap_prc)
 {
   cc_scloud_prc_t * p_prc = ap_prc;
   assert (p_prc);
-  delete_uri (p_prc);
   tiz_scloud_destroy (p_prc->p_sc_);
   p_prc->p_sc_ = NULL;
-  return OMX_ErrorNone;
+  return tiz_srv_super_deallocate_resources (typeOf (ap_prc, "cc_scloudprc"),
+                                             ap_prc);
 }
 
-static OMX_ERRORTYPE
-cc_scloud_prc_prepare_to_transfer (void * ap_prc, OMX_U32 a_pid)
+static const char *
+cc_scloud_prc_get_next_url (const void * p_obj)
 {
-  cc_scloud_prc_t * p_prc = ap_prc;
-  assert (ap_prc);
-  p_prc->eos_ = false;
-  return OMX_ErrorNone;
-}
-
-static OMX_ERRORTYPE
-cc_scloud_prc_transfer_and_process (void * ap_prc, OMX_U32 a_pid)
-{
-  cc_scloud_prc_t * p_prc = ap_prc;
-  OMX_ERRORTYPE rc = OMX_ErrorNone;
+  cc_scloud_prc_t * p_prc = (cc_scloud_prc_t *) p_obj;
   assert (p_prc);
-  return rc;
+  assert (p_prc->p_sc_);
+  return tiz_scloud_get_next_url (p_prc->p_sc_);
 }
 
-static OMX_ERRORTYPE
-cc_scloud_prc_stop_and_return (void * ap_prc)
+static const char *
+cc_scloud_prc_get_prev_url (const void * p_obj)
 {
-  cc_scloud_prc_t * p_prc = ap_prc;
+  cc_scloud_prc_t * p_prc = (cc_scloud_prc_t *) p_obj;
   assert (p_prc);
-  return release_buffer (p_prc);
+  assert (p_prc->p_sc_);
+  return tiz_scloud_get_prev_url (p_prc->p_sc_);
 }
 
-/*
- * from tizprc class
- */
-
-static OMX_ERRORTYPE
-cc_scloud_prc_buffers_ready (const void * ap_prc)
+static const char *
+cc_scloud_prc_get_current_song_album_art_url (const void * p_obj)
 {
-  cc_scloud_prc_t * p_prc = (cc_scloud_prc_t *) ap_prc;
+  cc_scloud_prc_t * p_prc = (cc_scloud_prc_t *) p_obj;
   assert (p_prc);
-  return OMX_ErrorNone;
+  assert (p_prc->p_sc_);
+  /*   return tiz_scloud_get_current_song_album_art (p_prc->p_sc_); */
+  return NULL;
 }
 
 static OMX_ERRORTYPE
-cc_scloud_prc_timer_ready (void * ap_prc, tiz_event_timer_t * ap_ev_timer,
-                           void * ap_arg, const uint32_t a_id)
-{
-  cc_scloud_prc_t * p_prc = ap_prc;
-  assert (p_prc);
-  return OMX_ErrorNone;
-}
-
-static OMX_ERRORTYPE
-cc_scloud_prc_pause (const void * ap_obj)
-{
-  return OMX_ErrorNone;
-}
-
-static OMX_ERRORTYPE
-cc_scloud_prc_resume (const void * ap_obj)
-{
-  return OMX_ErrorNone;
-}
-
-static OMX_ERRORTYPE
-cc_scloud_prc_port_flush (const void * ap_obj, OMX_U32 TIZ_UNUSED (a_pid))
+cc_scloud_prc_store_song_metadata (cc_scloud_prc_t * ap_obj)
 {
   cc_scloud_prc_t * p_prc = (cc_scloud_prc_t *) ap_obj;
-  return release_buffer (p_prc);
-}
-
-static OMX_ERRORTYPE
-cc_scloud_prc_port_disable (const void * ap_obj, OMX_U32 TIZ_UNUSED (a_pid))
-{
-  cc_scloud_prc_t * p_prc = (cc_scloud_prc_t *) ap_obj;
-  assert (p_prc);
-  p_prc->port_disabled_ = true;
-  /* Release any buffers held  */
-  return release_buffer ((cc_scloud_prc_t *) ap_obj);
-}
-
-static OMX_ERRORTYPE
-cc_scloud_prc_port_enable (const void * ap_prc, OMX_U32 a_pid)
-{
-  cc_scloud_prc_t * p_prc = (cc_scloud_prc_t *) ap_prc;
-  OMX_ERRORTYPE rc = OMX_ErrorNone;
-  assert (p_prc);
-  if (p_prc->port_disabled_)
-    {
-      p_prc->port_disabled_ = false;
-      if (p_prc->uri_changed_)
-        {
-          p_prc->uri_changed_ = false;
-        }
-    }
-  return rc;
-}
-
-static OMX_ERRORTYPE
-cc_scloud_prc_config_change (void * ap_prc, OMX_U32 TIZ_UNUSED (a_pid),
-                             OMX_INDEXTYPE a_config_idx)
-{
-  cc_scloud_prc_t * p_prc = ap_prc;
-  OMX_ERRORTYPE rc = OMX_ErrorNone;
-
+  cc_prc_t * p_cc_prc = (cc_prc_t *) p_prc;
   assert (p_prc);
 
-  if (OMX_TizoniaIndexConfigPlaylistSkip == a_config_idx && p_prc->p_cc_)
-    {
-      TIZ_INIT_OMX_STRUCT (p_prc->playlist_skip_);
-      tiz_check_omx (tiz_api_GetConfig (
-        tiz_get_krn (handleOf (p_prc)), handleOf (p_prc),
-        OMX_TizoniaIndexConfigPlaylistSkip, &p_prc->playlist_skip_));
-      p_prc->playlist_skip_.nValue > 0 ? obtain_next_url (p_prc, 1)
-                                       : obtain_next_url (p_prc, -1);
-      /* Changing the URL has the side effect of halting the current
-         download */
-      if (p_prc->port_disabled_)
-        {
-          /* Record that the URI has changed, so that when the port is
-             re-enabled, we restart the transfer */
-          p_prc->uri_changed_ = true;
-        }
-    }
-  return rc;
+  /* User and track title */
+  tiz_check_omx (cc_prc_store_song_metadata_item (
+    p_cc_prc, tiz_scloud_get_current_track_user (p_prc->p_sc_),
+    tiz_scloud_get_current_track_title (p_prc->p_sc_)));
+
+  /* Store the year if not 0 */
+  {
+    const char * p_year = tiz_scloud_get_current_track_year (p_prc->p_sc_);
+    if (p_year && strncmp (p_year, "0", 4) != 0)
+      {
+        tiz_check_omx (
+          cc_prc_store_song_metadata_item (p_cc_prc, "Year", p_year));
+      }
+  }
+
+  /* Duration */
+  tiz_check_omx (cc_prc_store_song_metadata_item (
+    p_cc_prc, "Duration",
+    tiz_scloud_get_current_track_duration (p_prc->p_sc_)));
+
+  /* Likes */
+  tiz_check_omx (cc_prc_store_song_metadata_item (
+    p_cc_prc, "Likes count",
+    tiz_scloud_get_current_track_likes (p_prc->p_sc_)));
+
+  /* Permalink */
+  tiz_check_omx (cc_prc_store_song_metadata_item (
+    p_cc_prc, "Permalink",
+    tiz_scloud_get_current_track_permalink (p_prc->p_sc_)));
+
+  /* License */
+  tiz_check_omx (cc_prc_store_song_metadata_item (
+    p_cc_prc, "License", tiz_scloud_get_current_track_license (p_prc->p_sc_)));
+
+  return OMX_ErrorNone;
 }
 
 /*
@@ -561,10 +303,10 @@ cc_scloud_prc_class_ctor (void * ap_obj, va_list * app)
 void *
 cc_scloud_prc_class_init (void * ap_tos, void * ap_hdl)
 {
-  void * tizprc = tiz_get_type (ap_hdl, "tizprc");
+  void * cc_prc = tiz_get_type (ap_hdl, "cc_prc");
   void * cc_scloudprc_class = factory_new
     /* TIZ_CLASS_COMMENT: class type, class name, parent, size */
-    (classOf (tizprc), "cc_scloudprc_class", classOf (tizprc),
+    (classOf (cc_prc), "cc_scloudprc_class", classOf (cc_prc),
      sizeof (cc_scloud_prc_class_t),
      /* TIZ_CLASS_COMMENT: */
      ap_tos, ap_hdl,
@@ -578,12 +320,12 @@ cc_scloud_prc_class_init (void * ap_tos, void * ap_hdl)
 void *
 cc_scloud_prc_init (void * ap_tos, void * ap_hdl)
 {
-  void * tizprc = tiz_get_type (ap_hdl, "tizprc");
+  void * cc_prc = tiz_get_type (ap_hdl, "cc_prc");
   void * cc_scloudprc_class = tiz_get_type (ap_hdl, "cc_scloudprc_class");
   TIZ_LOG_CLASS (cc_scloudprc_class);
   void * cc_scloudprc = factory_new
     /* TIZ_CLASS_COMMENT: class type, class name, parent, size */
-    (cc_scloudprc_class, "cc_scloudprc", tizprc, sizeof (cc_scloud_prc_t),
+    (cc_scloudprc_class, "cc_scloudprc", cc_prc, sizeof (cc_scloud_prc_t),
      /* TIZ_CLASS_COMMENT: */
      ap_tos, ap_hdl,
      /* TIZ_CLASS_COMMENT: class constructor */
@@ -595,27 +337,14 @@ cc_scloud_prc_init (void * ap_tos, void * ap_hdl)
      /* TIZ_CLASS_COMMENT: */
      tiz_srv_deallocate_resources, cc_scloud_prc_deallocate_resources,
      /* TIZ_CLASS_COMMENT: */
-     tiz_srv_prepare_to_transfer, cc_scloud_prc_prepare_to_transfer,
+     cc_prc_get_next_url, cc_scloud_prc_get_next_url,
      /* TIZ_CLASS_COMMENT: */
-     tiz_srv_transfer_and_process, cc_scloud_prc_transfer_and_process,
+     cc_prc_get_prev_url, cc_scloud_prc_get_prev_url,
      /* TIZ_CLASS_COMMENT: */
-     tiz_srv_stop_and_return, cc_scloud_prc_stop_and_return,
+     cc_prc_get_current_song_album_art_url,
+     cc_scloud_prc_get_current_song_album_art_url,
      /* TIZ_CLASS_COMMENT: */
-     tiz_srv_timer_ready, cc_scloud_prc_timer_ready,
-     /* TIZ_CLASS_COMMENT: */
-     tiz_prc_buffers_ready, cc_scloud_prc_buffers_ready,
-     /* TIZ_CLASS_COMMENT: */
-     tiz_prc_pause, cc_scloud_prc_pause,
-     /* TIZ_CLASS_COMMENT: */
-     tiz_prc_resume, cc_scloud_prc_resume,
-     /* TIZ_CLASS_COMMENT: */
-     tiz_prc_port_flush, cc_scloud_prc_port_flush,
-     /* TIZ_CLASS_COMMENT: */
-     tiz_prc_port_disable, cc_scloud_prc_port_disable,
-     /* TIZ_CLASS_COMMENT: */
-     tiz_prc_port_enable, cc_scloud_prc_port_enable,
-     /* TIZ_CLASS_COMMENT: */
-     tiz_prc_config_change, cc_scloud_prc_config_change,
+     cc_prc_store_song_metadata, cc_scloud_prc_store_song_metadata,
      /* TIZ_CLASS_COMMENT: stop value */
      0);
 
