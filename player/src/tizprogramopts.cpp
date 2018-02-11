a2955c7a677f8f238948843902f579e1f801a6c8
Juan A. Rubio <jarubio2001@gmail.com>
2018-02-11 12:06:19 +0000

    More copyright year updates

--- player/src/tizprogramopts.cpp
+++ player/src/tizprogramopts.cpp
@@ -420,7 +420,7 @@ int tiz::programopts::consume ()
 
 void tiz::programopts::print_version () const
 {
-  TIZ_PRINTF_BLU ("tizonia %s. Copyright (C) 2017 Juan A. Rubio\n",
+  TIZ_PRINTF_BLU ("tizonia %s. Copyright (C) 2018 Juan A. Rubio\n",
                   PACKAGE_VERSION);
   TIZ_PRINTF_BLU (
       "This software is part of the Tizonia project <http://tizonia.org>\n\n");
