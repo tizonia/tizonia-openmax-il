5e9e2c1ad391911ec1090fdbf59338b343959fd7
Juan A. Rubio <jarubio2001@gmail.com>
2018-11-28 23:57:28 +0000

    OMX_TizoniaExt.h: OMX_AUDIO_SpotifyPlaylistTypeFeaturedPlaylist

--- include/omxil-1.2/OMX_TizoniaExt.h
+++ include/omxil-1.2/OMX_TizoniaExt.h
@@ -281,6 +281,7 @@ typedef enum OMX_TIZONIA_AUDIO_SPOTIFYPLAYLISTTYPE {
     OMX_AUDIO_SpotifyPlaylistTypeAlbumId, /**< Album search by Spotify ID, URI or URL. */
     OMX_AUDIO_SpotifyPlaylistTypePlaylistId, /**< Playlist search by Spotify ID, URI or URL. */
     OMX_AUDIO_SpotifyPlaylistTypeRelatedArtists, /**< Related artists search. */
+    OMX_AUDIO_SpotifyPlaylistTypeFeaturedPlaylist, /**< Featured playlist search. */
     OMX_AUDIO_SpotifyPlaylistTypeKhronosExtensions = 0x6F000000, /**< Reserved region for introducing Khronos Standard Extensions */
     OMX_AUDIO_SpotifyPlaylistTypeVendorStartUnused = 0x7F000000, /**< Reserved region for introducing Vendor Extensions */
     OMX_AUDIO_SpotifyPlaylistTypeMax = 0x7FFFFFFF
