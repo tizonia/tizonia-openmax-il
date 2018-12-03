b3e68372e64ff04736cf6e9f41283ee71489e77d
Juan A. Rubio <jarubio2001@gmail.com>
2018-12-03 13:05:37 +0000

    OMX_TizoniaExt.h: added cAdditionalKeywords field to OMX_TIZONIA_AUDIO_PARAM_GMUSICPLAYLISTTYPE (part of #507)

--- include/omxil-1.2/OMX_TizoniaExt.h
+++ include/omxil-1.2/OMX_TizoniaExt.h
@@ -381,6 +381,7 @@ typedef struct OMX_TIZONIA_AUDIO_PARAM_GMUSICPLAYLISTTYPE {
     OMX_BOOL bShuffle;            /**< Default: OMX_FALSE */
     OMX_BOOL bUnlimitedSearch;    /**< Default: OMX_FALSE */
     OMX_U8 cPlaylistName[OMX_MAX_STRINGNAME_SIZE];
+    OMX_U8 cAdditionalKeywords[OMX_MAX_STRINGNAME_SIZE];
 } OMX_TIZONIA_AUDIO_PARAM_GMUSICPLAYLISTTYPE;
 
 
