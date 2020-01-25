6005cde1da24d0ae990cde7577ee447a0a91117c
Juan A. Rubio <jarubio2001@gmail.com>
2020-01-25 10:55:41 +0000

    tools/install.sh: fix for #670

--- tools/install.sh
+++ tools/install.sh
@@ -46,40 +46,40 @@ RELIDS=$(cat /etc/*-release)
 if echo "$RELIDS" | grep raspbian; then
     DISTRO="raspbian"
     if echo "$RELIDS" | grep buster; then
-        RELEASE="buster" ; PYVER=3
+        RELEASE="buster" ; PYVER=3 ; MOPRELEASE="buster"
     elif echo "$RELIDS" | grep stretch; then
-        RELEASE="stretch"; PYVER=2
+        RELEASE="stretch"; PYVER=2 ; MOPRELEASE="stretch"
     elif echo "$RELIDS" | grep jessie; then
-        RELEASE="jessie" ; PYVER=2
+        RELEASE="jessie" ; PYVER=2 ; MOPRELEASE="jessie"
     else
         echo "Can't find a supported Raspbian distribution."
         exit 1
     fi
 elif echo "$RELIDS" | grep stretch; then
-  DISTRO="debian" ; RELEASE="stretch" ; PYVER=2
+  DISTRO="debian" ; RELEASE="stretch" ; PYVER=2 ; MOPRELEASE="stretch"
 elif echo "$RELIDS" | grep -E 'buster'; then
-  DISTRO="debian" ; RELEASE="buster" ; PYVER=3
+  DISTRO="debian" ; RELEASE="buster" ; PYVER=3 ; MOPRELEASE="buster"
 elif echo "$RELIDS" | grep -E 'bullseye|kali-rolling'; then
   # NOTE: Kali Linux is based on Debian Testing, which is currently codenamed 'Bullseye'
-  DISTRO="debian" ; RELEASE="bullseye" ; PYVER=3
+  DISTRO="debian" ; RELEASE="bullseye" ; PYVER=3 ; MOPRELEASE="buster"
 elif echo "$RELIDS" | grep -E 'trusty|freya|qiana|rebecca|rafaela|rosa'; then
   # NOTE: Elementary OS 'freya' is based on trusty
   # NOTE: LinuxMint 'qiana' 'rebecca' 'rafaela' 'rosa' are all based on trusty
-  DISTRO="ubuntu" ; RELEASE="trusty" ; PYVER=2
+  DISTRO="ubuntu" ; RELEASE="trusty" ; PYVER=2 ; MOPRELEASE="jessie"
 elif echo "$RELIDS" | grep vivid; then
-  DISTRO="ubuntu" ; RELEASE="vivid" ; PYVER=2
+  DISTRO="ubuntu" ; RELEASE="vivid" ; PYVER=2 ; MOPRELEASE="jessie"
 elif echo "$RELIDS" | grep -E 'xenial|loki|sarah|serena|sonya|sylvia'; then
   # NOTE: Elementary OS 'loki' is based on xenial
   # NOTE: Linux Mint 'sarah', 'serena', 'sonya' and 'sylvia' are based on xenial
-  DISTRO="ubuntu" ; RELEASE="xenial" ; PYVER=2
-elif echo "$RELIDS" | grep -E 'bionic|juno|hera|tara|tessa|tina'; then
+  DISTRO="ubuntu" ; RELEASE="xenial" ; PYVER=2 ; MOPRELEASE="jessie"
+elif echo "$RELIDS" | grep -E 'bionic|juno|hera|tara|tessa|tina|tricia'; then
   # NOTE: Elementary OS 'juno', and 'hera' are based on bionic
-  # NOTE: Linux Mint 'tara'. 'tessa' and 'tina' are based on bionic
+  # NOTE: Linux Mint 'tara', 'tessa', 'tina', and 'tricia' are based on bionic
   # NOTE: Most of the time, binaries compiled on 18.04 will work on newer
   # releases, meaning you can try adding newer releases to the bionic conditional
-  # (e.g. 'disco|bionic|juno|...'), to support installation on a newer system; 
+  # (e.g. 'disco|bionic|juno|...'), to support installation on a newer system;
   # however, do this 'at your own risk', as not all features will be guaranteed to work.
-  DISTRO="ubuntu" ; RELEASE="bionic" ; PYVER=3
+  DISTRO="ubuntu" ; RELEASE="bionic" ; PYVER=3 ; MOPRELEASE="stretch"
 else
   echo "Can't find a supported Debian or Ubuntu-based distribution."
   exit 1
@@ -95,17 +95,17 @@ if [[ "$?" -ne 0 ]]; then
     exit 1
 fi
 
-# Add Mopidy's archive to APT's sources.list (required to install 'libspotify')
-grep -q "apt.mopidy.com" /etc/apt/sources.list
-if [[ "$?" -eq 1 ]]; then
-    curl 'http://apt.mopidy.com/mopidy.gpg' | sudo apt-key add -
-    echo "deb http://apt.mopidy.com/ stable main contrib non-free" | sudo tee -a /etc/apt/sources.list
+# Add Mopidy's APT archive
+if [[ ! -f /etc/apt/sources.list.d/mopidy.list ]]; then
+    echo "Setting up Mopidy's APT archive for $DISTRO:$MOPRELEASE (to install 'libspotify')"
+    curl 'https://apt.mopidy.com/mopidy.gpg' | sudo apt-key add -
+    sudo curl https://apt.mopidy.com/$MOPRELEASE.list -o /etc/apt/sources.list.d/mopidy.list
 fi
 
-# Add Tizonia's archive to APT's sources.list
+# Add Tizonia's APT archive
 grep -q "dl.bintray.com/tizonia" /etc/apt/sources.list
 if [[ "$?" -eq 1 ]]; then
-    echo "Setting up Tizonia's Bintray archive for $DISTRO:$RELEASE"
+    echo "Setting up Tizonia's Bintray APT archive for $DISTRO:$RELEASE"
     curl -k 'https://bintray.com/user/downloadSubjectPublicKey?username=tizonia' | sudo apt-key add -
     echo "deb https://dl.bintray.com/tizonia/$DISTRO $RELEASE main" | sudo tee -a /etc/apt/sources.list
 fi
