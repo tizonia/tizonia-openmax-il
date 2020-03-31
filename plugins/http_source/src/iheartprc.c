55b3b77e933b06c649b1fe75557e02dcec79aa8d
Juan A. Rubio <jarubio2001@gmail.com>
2020-03-31 16:03:34 +0100

    plugins/http_source: 'Audio Encoding'

--- plugins/http_source/src/iheartprc.c
+++ plugins/http_source/src/iheartprc.c
@@ -355,6 +355,11 @@ update_metadata (iheart_prc_t * ap_prc)
     store_metadata (ap_prc, "State",
                     tiz_iheart_get_current_radio_state (ap_prc->p_iheart_)));
 
+  /* Audio Encoding */
+  tiz_check_omx (
+    store_metadata (ap_prc, "Audio Encoding",
+                    tiz_iheart_get_current_radio_audio_encoding (ap_prc->p_iheart_)));
+
   /* Website */
   tiz_check_omx (
     store_metadata (ap_prc, "Website",
