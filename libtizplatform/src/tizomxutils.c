9a67004ccdee62863033478096eb6cc3a9cf3ab7
Juan A. Rubio <jarubio2001@gmail.com>
2020-05-04 23:08:50 +0100

    libtizplatform: added 'OMX_TizoniaIndexConfigPlaylistPosition' to omxutils

--- libtizplatform/src/tizomxutils.c
+++ libtizplatform/src/tizomxutils.c
@@ -663,6 +663,8 @@ static const tiz_idx_str_t tiz_idx_to_str_tbl[] = {
    (const OMX_STRING) "OMX_TizoniaIndexParamAudioIheartSession"},
   {OMX_TizoniaIndexParamAudioIheartPlaylist,
    (const OMX_STRING) "OMX_TizoniaIndexParamAudioIheartPlaylist"},
+  {OMX_TizoniaIndexConfigPlaylistPosition,
+   (const OMX_STRING) "OMX_TizoniaIndexConfigPlaylistPosition"},
   {OMX_IndexKhronosExtensions, (const OMX_STRING) "OMX_IndexKhronosExtensions"},
   {OMX_IndexVendorStartUnused, (const OMX_STRING) "OMX_IndexVendorStartUnused"},
   {OMX_IndexMax, (const OMX_STRING) "OMX_IndexMax"}};
